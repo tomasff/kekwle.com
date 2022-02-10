81965c1bc187f017259a64e809af5c101955b0d0
Tomás F <tomasff02@gmail.com>
2022-02-10 23:03:45 +0000

    Added a coming soon page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons'
+import { faCog } from '@fortawesome/free-solid-svg-icons'
+
+import kekw from './kekw.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+    <div className='container'>
+      <div className='header'>
+        <div className='left'>
+          <div className='menuIcon'>
+            <FontAwesomeIcon icon={faQuestionCircle} />
+          </div>
+        </div>
+        <div className='center'>
           KEKWLE
-        </a>
-      </header>
+        </div>
+        <div className='right'>
+          <div className='menuIcon'>
+            <FontAwesomeIcon icon={faCog} />
+          </div>
+        </div>
+      </div>
+      <div className='game'>
+        <img src={kekw} alt='KEKW image' />
+        <h2 id='comingSoon'>Coming Soon</h2>
+      </div>
+      <div className='footer'>
+        Kekwle &middot; source code available on <a href='https://github.com/tomasff/kekwle.com'>GitHub</a>
+      </div>
     </div>
   );
 }
