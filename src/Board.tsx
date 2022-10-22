855571081fcdf923c8157fc1555affcdcc88d34c
Tomás F <15079464+tomasff@users.noreply.github.com>
2022-10-22 14:15:52 +0100

    Guesses

--- src/Board.tsx
+++ src/Board.tsx
@@ -5,19 +5,35 @@ import { useEffect, useState } from 'react';
 interface BoardProps {
     numberOfGuesses: number;
     targetWord: string;
-
 }
 
 const Board = ({ targetWord, numberOfGuesses }: BoardProps) => {
+    const [currentGuess, setCurrentGuess] = useState<number>(0);
 
-    const [currentGuess, setCurrentGuess] = useState<string>('');
-    const [currentRow, setCurrentRow] = useState<number>(0);
+    const [guesses, setGuesses] = useState<string[]>(
+        Array(numberOfGuesses).fill('')
+    );
+
+    const getCurrentGuess = () => guesses[currentGuess];
+
+    const updateCurrentGuess = (guess: string) => {
+        const updatedGuesses = [...guesses];
+
+        updatedGuesses[currentGuess] = guess;
+
+        setGuesses(updatedGuesses);
+    }
 
     const onKeyPress = (e: KeyboardEvent) => {
-        if (targetWord.includes(e.key)) {
-            setCurrentGuess(currentGuess + e.key);
-        } else if (e.key == 'Backspace') {
-            setCurrentGuess(currentGuess.slice(0, -1));
+        console.log(e.key);
+
+        if (targetWord.includes(e.key) &&
+            getCurrentGuess().length < targetWord.length) {
+            updateCurrentGuess(getCurrentGuess() + e.key);
+        } else if (e.key === 'Backspace') {
+            updateCurrentGuess(getCurrentGuess().slice(0, -1));
+        } else if (e.key === 'Enter') {
+            setCurrentGuess(currentGuess + 1);
         }
     };
 
@@ -31,10 +47,12 @@ const Board = ({ targetWord, numberOfGuesses }: BoardProps) => {
 
     return (
         <div className="board">
-            {/* {Array(numberOfGuesses).map(
-                _ => <GuessRow wordLength={} />
-            )} */}
-            <GuessRow currentGuess={currentGuess} wordLength={targetWord.length} />
+            {guesses.map(
+                guess => <GuessRow
+                    currentGuess={guess}
+                    wordLength={targetWord.length}
+                />
+            )}
         </div>
     );
 }
--- src/GuessRow.tsx
+++ src/GuessRow.tsx
@@ -1,10 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+
 interface GuessRowProps {
     currentGuess: string;
     wordLength: number;
 }
 
 const GuessRow = ({ currentGuess, wordLength }: GuessRowProps) => {
-
     const letterAtPosition = (position: number) => {
         if (position > currentGuess.length) {
             return ' ';
@@ -19,6 +21,9 @@ const GuessRow = ({ currentGuess, wordLength }: GuessRowProps) => {
                 <div className='boardTile'>
                     {letterAtPosition(i)}
                 </div>)}
+            {/* <div className='boardIconTile'>
+                <FontAwesomeIcon className='loadingIcon' size='2x' icon={faSpinner} spin />
+            </div> */}
         </div>
     );
 }
